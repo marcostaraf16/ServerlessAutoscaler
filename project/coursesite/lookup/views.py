f45f6eb61b93496c5eabd62bd228f1226f321027
RealUGA <49420699+RealUGA@users.noreply.github.com>
2021-10-08 19:45:00 -0400

    Added generic views for Course example

--- project/coursesite/lookup/views.py
+++ project/coursesite/lookup/views.py
@@ -1,3 +1,14 @@
+from django.views import generic
 from django.shortcuts import render
+from .models import Course
 
-# Create your views here.
+class IndexView(generic.ListView):
+   template_name = 'lookup/index.html'
+   context_object_name = 'all_courses'
+
+   def get_queryset(self):
+       return Course.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Course
+    template_name = 'lookup/detail.html'
