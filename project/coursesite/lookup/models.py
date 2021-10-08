9662e828c85a6d3e1f20964300f4cc0c96b2819e
RealUGA <49420699+RealUGA@users.noreply.github.com>
2021-10-08 19:38:13 -0400

    Course model example

--- project/coursesite/lookup/models.py
+++ project/coursesite/lookup/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+import datetime
 
 # Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=20)
+    professor = models.CharField(max_length=50)
+    days = models.CharField(max_length=5)
+    class_size = models.IntegerField()
